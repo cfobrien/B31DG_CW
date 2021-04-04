2133ca98dbc89ed96c7ad8153eaeab7335668a4f
cfobrien <conor.obrien24@gmail.com>
2021-04-04 03:50:08 +0100

    work on rolling average task

--- Motor_Controller/Motor_Controller.ino
+++ Motor_Controller/Motor_Controller.ino
@@ -2,9 +2,16 @@
 #include<semphr.h>
 
 SemaphoreHandle_t serial_sem;
-SemaphoreHandle_t left_motor_sem;
-SemaphoreHandle_t right_motor_sem;
+SemaphoreHandle_t left_motor_sem, right_motor_sem;
 SemaphoreHandle_t diag_mode_sem;
+SemaphoreHandle_t roll_avg_sem;
+
+typedef struct roll_avg_t {
+  SemaphoreHandle_t roll_avg_sem;
+  uint16_t avg;
+  uint8_t count;
+  uint8_t width;
+} roll_avg_t;
 
 typedef struct motor_t {
   SemaphoreHandle_t motor_sem;
@@ -19,6 +26,7 @@ volatile motor_t left_motor, right_motor;
 TaskHandle_t sensor_processing_th;
 TaskHandle_t left_motor_controller_th;
 TaskHandle_t right_motor_controller_th;
+TaskHandle_t update_rolling_avg_th;
 
 void task_sensor_processing(void *params);
 void task_motor_controller(void *params);
@@ -27,7 +35,9 @@ void setup() {
   serial_sem = xSemaphoreCreateMutex();
   left_motor_sem = xSemaphoreCreateMutex();
   right_motor_sem = xSemaphoreCreateMutex();
+  roll_avg_sem = xSemaphoreCreateMutex();
 
+  roll_avg_t roll_avg = { .roll_avg_sem = roll_avg_sem, .avg = 0, .count = 0, .width = 6 };
   left_motor = { .motor_sem = left_motor_sem, .max_vel = 1023, .warn_vel = (uint16_t) 0.88*1023, .vel = 0, .vel_increment = (uint16_t) 0.2 * 1023 };
   right_motor = { .motor_sem = right_motor_sem, .max_vel = 1023, .warn_vel = (uint16_t) 0.88*1023, .vel = 0, .vel_increment = (uint16_t) 0.2 * 1023 };
   
@@ -37,14 +47,42 @@ void setup() {
   xTaskCreate(task_sensor_processing, "Sensor Processing task", 128, NULL, 1, &sensor_processing_th);
   xTaskCreate(task_motor_controller, "Left Motor Controller", 128, (void *)&left_motor, 1, &left_motor_controller_th);
   xTaskCreate(task_motor_controller, "Right Motor Controller", 128, (void *)&right_motor, 1, &right_motor_controller_th);
+  xTaskCreate(task_update_rolling_avg, "Update rolling average", 128, (void *)&roll_avg, 1, &update_rolling_avg_th);
 }
 
 void loop() {}
 
-void task_motor_controller(void *params) {
+void task_update_rolling_avg(void *params) {
+  roll_avg_t * roll_avg;
+  uint8_t count;
+  TickType_t delay = 500 / portTICK_PERIOD_MS;
   for (;;) {
-    motor_t * motor = (motor_t *)params;
-    uint16_t max_vel;
+    roll_avg = (roll_avg_t *)params;
+    if (roll_avg->roll_avg_sem != NULL) {
+      if (xSemaphoreTake(roll_avg->roll_avg_sem, (TickType_t) 5 == pdTRUE)) {
+//        roll_avg->count += 1 % roll_avg->width;
+        count = roll_avg->count;
+        count += 1 % roll_avg->width;
+        count = roll_avg->width;
+        roll_avg->count = count;
+        xSemaphoreGive(roll_avg->roll_avg_sem);
+      }
+    }
+    if (serial_sem != NULL) {
+      if (xSemaphoreTake(serial_sem, (TickType_t) 5 == pdTRUE)) {
+        Serial.println(count);
+        xSemaphoreGive(serial_sem);
+      }
+    }
+    vTaskDelay(delay); //0.5s busy wait (2Hz rate)
+  }
+}
+
+void task_motor_controller(void *params) {
+  motor_t * motor;
+  uint16_t max_vel;
+  for (;;) {
+    motor = (motor_t *)params;
     if (motor->motor_sem != NULL) {
       if (xSemaphoreTake(motor->motor_sem, (TickType_t) 5 == pdTRUE)) {
         max_vel = motor->max_vel;
